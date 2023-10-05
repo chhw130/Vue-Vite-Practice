f7c2dc15ec4b4acd472c6569697998215671622e
chhw130 <527coco@naver.com>
2023-10-05 19:34:40 +0900

    Feat : goListPage Function on PstCreateView Component

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
-    <h2>게시르 수정</h2>
+    <h2>게시글 수정</h2>
+    <hr class="my-4" />
+    <form>
+      <div class="mb-3">
+        <label for="title" class="form-label">제목</label>
+        <input type="text" class="form-control" id="title" />
+      </div>
+      <div class="mb-3">
+        <label for="content" class="form-label">내용</label>
+        <textarea class="form-control" id="content" rows="3"></textarea>
+      </div>
+      <div class="pt-4">
+        <button type="button" class="btn btn-outline-danger me-2">취소</button>
+        <button class="btn btn-primary" @click="goDetailPage">수정</button>
+      </div>
+    </form>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+const id = route.params.id
+
+const goDetailPage = () => {
+  return router.push({ name: 'PostDetail', params: { id } })
+}
+</script>
 
 <style lang="scss" scoped></style>
