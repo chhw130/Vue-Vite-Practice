061c2103e1723559bf2da498e9e68a1b00c35e07
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-10-09 17:09:26 +0900

    Feat : env File

--- components.d.ts
+++ components.d.ts
@@ -1,8 +1,11 @@
-declare module '@vule/runtime-core'{
+declare module '@vule/runtime-core' {
   export interface GlobalComponents {
-    RouterLink : typeof import('vue-router')['RouterLink']
-    RouterView : typeof import('vue-router')['RouterView']
+    RouterLink: (typeof import('vue-router'))['RouterLink']
+    RouterView: (typeof import('vue-router'))['RouterView']
+    env: {
+      VITE_APP_API_URL: string
+    }
   }
 }
 
-export{}
+export {}
--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,42 +1,7 @@
 import axios from 'axios'
-import { Ref } from 'vue'
-
-export const posts = [
-  {
-    id: 1,
-    title: '제목1',
-    createdAt: '2020-01-01',
-    content: '내용1'
-  },
-  {
-    id: 2,
-    title: '제목2',
-    createdAt: '2020-01-02',
-    content: '내용2'
-  },
-  {
-    id: 3,
-    title: '제목3',
-    content: '내용3',
-
-    createdAt: '2020-01-03'
-  },
-  {
-    id: 4,
-    title: '제목4',
-    content: '내용4',
-    createdAt: '2020-01-04'
-  },
-  {
-    id: 5,
-    title: '제목5',
-    content: '내용5',
-    createdAt: '2020-01-05'
-  }
-]
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: import.meta.env.BASE_URL
 })
 
 export interface PostData {
