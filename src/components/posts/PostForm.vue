740b99c1ecd78febc54c777d0bb6c7c0cdfed680
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-10-09 18:03:34 +0900

    Refactor : Split component

--- src/components/posts/PostForm.vue
+++ src/components/posts/PostForm.vue
@@ -2,24 +2,20 @@
   <form>
     <div class="mb-3">
       <label for="title" class="form-label">제목</label>
-      <input
-        type="text"
-        class="form-control"
-        id="title"
-        @input="$emit('update:title', $event.target.value)"
-      />
+      <input :value="title" type="text" class="form-control" id="title" @input="updateTitle" />
     </div>
     <div class="mb-3">
       <label for="content" class="form-label">내용</label>
       <textarea
+        :value="content"
         class="form-control"
         id="content"
         rows="3"
-        @input="$emit('update:content', $event.target.value)"
+        @input="updateContent"
       ></textarea>
     </div>
     <div class="pt-4">
-      <button type="button" class="btn btn-outline-danger me-2">취소</button>
+      <button type="button" class="btn btn-outline-danger me-2" @click="goDetailPage">취소</button>
       <button class="btn btn-primary">수정</button>
     </div>
     <!-- <slot name="actions"> </slot> -->
@@ -38,7 +34,15 @@ defineProps({
   title: String,
   content: String
 })
-defineEmits(['update:title', 'update:content'])
+const emit = defineEmits(['update:title', 'update:content'])
+
+const updateTitle = (e: Event) => {
+  emit('update:title', (e.target as HTMLInputElement).value)
+}
+
+const updateContent = (e: Event) => {
+  emit('update:content', (e.target as HTMLInputElement).value)
+}
 
 const goDetailPage = () => {
   return router.push({ name: 'PostDetail', params: { id } })
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,17 +2,20 @@
   <div>
     <h2>게시글 수정</h2>
     <hr class="my-4" />
-    <PostForm :title="formData.title" :content="formData.content" @submit="editPost"></PostForm>
+    <PostForm
+      v-model:title="formData.title"
+      v-model:content="formData.content"
+      @submit.prevent="editPost"
+    ></PostForm>
   </div>
 </template>
 
 <script setup lang="ts">
 import { PostData, getPostById, updatePost } from '@/api/posts'
 import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import PostForm from '@/components/posts/PostForm.vue'
 
-const router = useRouter()
 const route = useRoute()
 const id = route.params.id
 
@@ -33,12 +36,10 @@ const setForm = (data: PostData) => {
 }
 
 const editPost = async () => {
-  console.log(formData.value)
   await updatePost(id, formData.value)
 }
 
 fetchPost()
-
 </script>
 
 <style lang="scss" scoped></style>
